0b7252fa8c3a6e4c0cef41bd298ac9f51e912b2a
VIKASH KUUMAR GUPTA <vikashgpt16@gmail.com>
2022-11-28 15:28:29 +0530

    Update App.js
    updated

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       <Routes>
         <Route path='/' element = {<Home/>} />
         <Route path='/About' element = {<About/>} />
